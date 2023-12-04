510a78f5c18a60e346d3a2c523b93b35aa5d06fe
Camibrit <camila-brito10@hotmail.com>
2023-12-04 10:57:01 -0300

    atualizações

--- src/main/java/com/AppRH/controllers/VagaCcontroller.java
+++ src/main/java/com/AppRH/controllers/VagaCcontroller.java
@@ -4,12 +4,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.AppRH.AppRH.models.Candidato;
 import com.AppRH.AppRH.models.Vaga;
 import com.AppRH.AppRH.repository.CandidatoRepository;
